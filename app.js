08ea01229c3aeb1308f502e345bd4e08bcb67238
Dominique Laurent <dolau@videotron.ca>
2022-07-24 19:45:45 -0400

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
 
 
-
-
-const navToggle = document.querySelector('.navToggle');
-const navlinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-	document.body.classList.toggle('nav-open');
-});
-
-
-navlinks.forEach(link => {
-		link.addEventListener('click', () => {
-			document.body.classList.remove('nav-open')
-		})
-})
-
